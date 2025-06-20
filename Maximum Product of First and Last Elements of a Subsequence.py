c67ea7adf0f383b40f395da510b10579abe90848
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-06-20 19:02:21 +0530

    Update Maximum Product of First and Last Elements of a Subsequence.py

--- Maximum Product of First and Last Elements of a Subsequence.py
+++ Maximum Product of First and Last Elements of a Subsequence.py
@@ -1,1 +1,11 @@
+class Solution:
+    def maximumProduct(self, nums, m):
+        n = len(nums)
+        max_product = float('-inf')
 
+        for i in range(n - m + 1):
+            for j in range(i + m - 1, n):
+                product = nums[i] * nums[j]
+                max_product = max(max_product, product)
+
+        return max_product
